3e4b6bbb13f6833706c8c8142adc206522c1a235
StivenLind <125386504+StivenLind@users.noreply.github.com>
2023-11-21 17:53:49 -0500

    Documentación Clase Nodo

--- Biblioteca/src/main/java/com/mycompany/biblioteca/Nodo.java
+++ Biblioteca/src/main/java/com/mycompany/biblioteca/Nodo.java
@@ -8,12 +8,22 @@ import java.io.Serializable;
 
 
 /**
- *
+ * Clase Nodo para representar un elemento en una lista doblemente enlazada.
+ * Cada nodo contiene un libro, una referencia al siguiente nodo y una referencia al nodo anterior.
+ * 
  * @author ADMIN
  */
 public class Nodo implements Serializable {
     public Libro libro;
     Nodo siguiente, anterior;
+    
+    /**
+     * Constructor de la clase Nodo.
+     * 
+     * @param libro    Libro que se almacenará en el nodo.
+     * @param ant      Nodo anterior en la lista.
+     * @param sig      Nodo siguiente en la lista.
+     */
     
     public Nodo ( Libro libro, Nodo ant, Nodo sig){
         this.libro=libro;
@@ -21,19 +31,43 @@ public class Nodo implements Serializable {
         anterior = ant;
         
     }
-
+    
+    /**
+     * Obtiene el siguiente nodo en la lista.
+     * 
+     * @return Referencia al siguiente nodo.
+     */
+    
     public Nodo getSiguiente() {
         return siguiente;
     }
-
+    
+    /**
+     * Establece el siguiente nodo en la lista.
+     * 
+     * @param siguiente Nodo siguiente en la lista.
+     */
+    
     public void setSiguiente(Nodo siguiente) {
         this.siguiente = siguiente;
     }
 
+    /**
+     * Obtiene el nodo anterior en la lista.
+     * 
+     * @return Referencia al nodo anterior.
+     */
+    
     public Nodo getAnterior() {
         return anterior;
     }
 
+     /**
+     * Establece el nodo anterior en la lista.
+     * 
+     * @param anterior Nodo anterior en la lista.
+     */
+    
     public void setAnterior(Nodo anterior) {
         this.anterior = anterior;
     }
